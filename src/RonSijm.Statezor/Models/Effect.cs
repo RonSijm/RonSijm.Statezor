8f6fce6ac0096c405fa4cc82bb2a80acd286643d
RonSijm <ron@appical.nl>
2024-01-09 21:45:11 +0100

    - Made currentstate available for reducers

--- src/RonSijm.Statezor/Models/Effect.cs
+++ src/RonSijm.Statezor/Models/Effect.cs
@@ -11,16 +11,16 @@ public abstract class Reducer<TOut, T> : Effect<T>
 {
     public override Func<Type, bool> TypeCriteria => type => type == typeof(TOut);
 
-    private readonly IState<TOut> _currentState;
+    public IState<TOut> CurrentState { get; }
 
     protected Reducer(IState<TOut> currentState)
     {
-        _currentState = currentState;
+        CurrentState = currentState;
     }
 
     public override Func<T, Task> Action => input =>
     {
-        _currentState.Publish(GetValue(input));
+        CurrentState.Publish(GetValue(input));
         return Task.CompletedTask;
     };
 
